ae927c95f00419c7999cd2808b103450740909d2
Szymon Klimek <kliszym@gmail.com>
2019-08-10 01:20:32 +0200

    f commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace FFMPEGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(386, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(334, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(46, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(414, 287);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(646, 45);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(183, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "different output filename";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(581, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Chosen files:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(479, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "New filename:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(594, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 48);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Start Conversion";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Location = new System.Drawing.Point(660, 156);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(115, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(479, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Choose new file extension:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(479, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "New path:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(482, 91);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(293, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(781, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Browse...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(482, 45);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(158, 21);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "different output path";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 385);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FFMPEGUI
+{
+    public partial class Form1 : Form
+    {
+
+        public string newFileName;
+        public string defaultFileName;
+        public string defaultName;
+        public string defaultDirectory;
+        public string editedFileName;
+        public string editedName;
+        public string editedDirectory;
+        public string oldFileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(new object[] { ".avi", ".mp4", ".mpeg4", ".mts" });
+            comboBox1.SelectedIndex = 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.CheckPathExists = true;
+            fileDialog.CheckFileExists = true;
+            //tylko jeden film póki co 
+            fileDialog.Multiselect = false;
+
+            fileDialog.Title = "Browse Video Files";
+            fileDialog.Filter
+                = "Video Files|*.mpeg4;*.avi;*.mkv;*.mp4;*.mts" ;
+            fileDialog.ShowDialog();
+
+            oldFileName = textBox1.Text = fileDialog.FileName;
+
+            char[] separator = { '.' };
+            
+            String[] separated= fileDialog.FileName.Split(separator);
+
+            defaultFileName = separated[0];
+            for (int i = 1; i < separated.Length - 1; i++)
+            {
+                defaultFileName = defaultFileName + "." + separated[i];
+            }
+
+            editedFileName = defaultFileName;
+
+            char[] separatorName = { '\\' };
+
+            String[] separatedFolders = defaultFileName.Split(separatorName);
+
+            defaultDirectory = separatedFolders[0];
+            for(int i = 1; i < separatedFolders.Length - 1; i++)
+            {
+                defaultDirectory = defaultDirectory + "\\" + separatedFolders[i];
+            }
+            defaultName = separatedFolders[separatedFolders.Length - 1];
+            
+            textBox2.Text = defaultName;
+            textBox3.Text = defaultDirectory;
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                textBox2.Enabled = true;
+                textBox2.Text = editedName;
+            }
+            else
+            {
+                textBox2.Enabled = false;
+                editedName = textBox2.Text;
+                textBox2.Text = defaultName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            oldFileName = "\"" + textBox1.Text + "\"";
+            newFileName = "\"" + textBox3.Text + textBox2.Text + comboBox1.SelectedItem.ToString() + "\"";
+
+            if (oldFileName == "\"\"")
+            {
+                MessageBox.Show("Source file name can't be empty");
+                return;
+            }
+            if(newFileName == "\"" + comboBox1.SelectedItem.ToString() + "\"")
+            {
+                MessageBox.Show("Destination file name can't be empty");
+                return;
+            }
+
+            ProcessStartInfo info = new ProcessStartInfo("ffmpeg.exe");
+
+            info.Arguments = "-i " + oldFileName + " " + newFileName;
+
+            MessageBox.Show(info.Arguments);
+
+            info.UseShellExecute = false;
+            var process = new Process();
+            process.StartInfo = info;
+            process.Start();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+
+            folderDialog.ShowNewFolderButton = true;
+            folderDialog.ShowDialog();
+            string temp = folderDialog.SelectedPath;
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                textBox3.Enabled = true;
+                textBox3.Text = editedDirectory;
+            }
+            else
+            {
+                textBox3.Enabled = false;
+                editedDirectory = textBox3.Text;
+                textBox3.Text = defaultDirectory;
+            }
+        }
+    }
+}
